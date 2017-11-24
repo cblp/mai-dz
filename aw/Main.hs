50141d27bd4db7bf2f6c6aacab264ebf1d06c5f5
Yuriy Syrovetskiy <cblp@cblp.su>
2017-11-24 19:30:22 +0300

    Use custom qtah

--- aw/Main.hs
+++ aw/Main.hs
@@ -3,17 +3,14 @@
 import           Foreign.Hoppy.Runtime (withScopedPtr)
 import           Graphics.UI.Qtah.Core.QCoreApplication as QCoreApplication
 import           Graphics.UI.Qtah.Widgets.QApplication as QApplication
-import           Graphics.UI.Qtah.Widgets.QFrame as QFrame
-import           Graphics.UI.Qtah.Widgets.QLabel as QLabel
-import           Graphics.UI.Qtah.Widgets.QWidget as QWidget
+import           Graphics.UI.Qtah.Widgets.QTreeWidget as QTreeWidget
+import qualified Graphics.UI.Qtah.Widgets.QWidget as QWidget
 import           System.Environment (getArgs)
 
 main :: IO ()
-main = withScopedPtr (getArgs >>= QApplication.new) mainApp
-
-mainApp :: QApplication -> IO ()
-mainApp _ = do
-    hello <- newWithText "hello"
-    setFrameShape hello StyledPanel
-    QWidget.show hello
+main = withApp $ \_ -> do
+    window <- QTreeWidget.new
+    QWidget.show window
     exec
+  where
+    withApp = withScopedPtr $ getArgs >>= QApplication.new
