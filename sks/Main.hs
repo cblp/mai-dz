0ef63fb77ff50460acb4a6c1892d51116b7ad9e1
Yuriy Syrovetskiy <cblp@cblp.su>
2017-12-07 18:12:48 +0300

    Add empty handlers to buttons

--- sks/Main.hs
+++ sks/Main.hs
@@ -1,18 +1,20 @@
 -- http://khumba.net/docs/qtah-qt5-0.3.0
 
 import           Foreign.Hoppy.Runtime (withScopedPtr)
-import           Graphics.UI.Qtah.Core.QCoreApplication as QCoreApplication
-import           Graphics.UI.Qtah.Core.QSize as QSize
-import           Graphics.UI.Qtah.Gui.QIcon as QIcon
-import           Graphics.UI.Qtah.Widgets.QAbstractButton (setIconSize)
-import           Graphics.UI.Qtah.Widgets.QApplication as QApplication
-import           Graphics.UI.Qtah.Widgets.QBoxLayout as QBoxLayout
-import           Graphics.UI.Qtah.Widgets.QHBoxLayout as QHBoxLayout
-import           Graphics.UI.Qtah.Widgets.QLayout as QLayout
-import           Graphics.UI.Qtah.Widgets.QPushButton as QPushButton
-import           Graphics.UI.Qtah.Widgets.QTreeView as QTreeView
-import           Graphics.UI.Qtah.Widgets.QTreeWidget as QTreeWidget
-import           Graphics.UI.Qtah.Widgets.QVBoxLayout as QVBoxLayout
+import           Graphics.UI.Qtah.Core.QCoreApplication (exec)
+import qualified Graphics.UI.Qtah.Core.QSize as QSize
+import qualified Graphics.UI.Qtah.Gui.QIcon as QIcon
+import           Graphics.UI.Qtah.Signal (connect_)
+import           Graphics.UI.Qtah.Widgets.QAbstractButton (pressedSignal,
+                                                           setIconSize)
+import qualified Graphics.UI.Qtah.Widgets.QApplication as QApplication
+import           Graphics.UI.Qtah.Widgets.QBoxLayout (addStretch, addWidget)
+import qualified Graphics.UI.Qtah.Widgets.QHBoxLayout as QHBoxLayout
+import           Graphics.UI.Qtah.Widgets.QLayout (addItem)
+import qualified Graphics.UI.Qtah.Widgets.QPushButton as QPushButton
+import           Graphics.UI.Qtah.Widgets.QTreeView (setHeaderHidden)
+import qualified Graphics.UI.Qtah.Widgets.QTreeWidget as QTreeWidget
+import qualified Graphics.UI.Qtah.Widgets.QVBoxLayout as QVBoxLayout
 import           Graphics.UI.Qtah.Widgets.QWidget (QWidget, setLayout)
 import qualified Graphics.UI.Qtah.Widgets.QWidget as QWidget
 import           System.Environment (getArgs)
@@ -39,36 +41,32 @@ makeAppWindow = do
     mainLayout <- QHBoxLayout.new
     setLayout appWindow mainLayout
 
-    let addButton icon text layout = do
+    let addButton icon text layout handler = do
             button <-
                 QPushButton.newWithIconAndTextAndParent icon text appWindow
             setIconSize button buttonIconSize
-            QBoxLayout.addWidget layout button
+            addWidget layout button
+            connect_ button pressedSignal handler
 
     do  leftPanel <- QVBoxLayout.new
         addItem mainLayout leftPanel
 
-        _addVerticalCablingButton <-
-            addButton connectionV "Добавить\nвертикальную подсистему" leftPanel
-        _addHorizontalCablingButton <-
-            addButton
-                connectionH "Добавить\nгоризонтальную подсистему" leftPanel
-        _addWorkPlaceButton <-
-            addButton laptop "Добавить\nрабочее место" leftPanel
+        addButton connectionV "Добавить\nвертикальную подсистему" leftPanel $
+            pure ()
+        addButton connectionH "Добавить\nгоризонтальную подсистему" leftPanel $
+            pure ()
+        addButton laptop "Добавить\nрабочее место" leftPanel $ pure ()
         addStretch leftPanel
 
     do  workArea <- QTreeWidget.newWithParent appWindow
         setHeaderHidden workArea True
-        QBoxLayout.addWidget mainLayout workArea
+        addWidget mainLayout workArea
 
     do  rightPanel <- QVBoxLayout.new
         addItem mainLayout rightPanel
 
         addStretch rightPanel
-        _saveTextButton <-
-            addButton fileList "Сохранить\nтекстовое описание" rightPanel
-        _saveImageButton <-
-            addButton fileImage "Сохранить\nизображение" rightPanel
-        pure ()
+        addButton fileList "Сохранить\nтекстовое описание" rightPanel $ pure ()
+        addButton fileImage "Сохранить\nизображение" rightPanel $ pure ()
 
     pure appWindow
