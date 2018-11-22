854c5e64c720a16f4a23f6366a597d8ab0674c60
Yuriy Syrovetskiy <cblp@cblp.su>
2018-11-22 16:05:54 +0300

    Add rectangle-cli implementation

--- rectangle/Cli.hs
+++ rectangle/Cli.hs
@@ -1,2 +1,13 @@
+import           Data.Maybe (mapMaybe)
+import           System.Environment (getArgs, getProgName)
+import           Text.Read (readMaybe)
+
 main :: IO ()
-main = pure ()
+main = do
+    prog   <- getProgName
+    args   <- getArgs
+    (a, b) <- case mapMaybe readMaybe args of
+        [a, b] -> pure (a, b :: Double)
+        _      -> fail ("Usage: " ++ prog ++ " NUMBER NUMBER")
+    putStrLn ("perimeter: " ++ show (2 * (a + b)))
+    putStrLn ("diagonal: " ++ show (sqrt (a * a + b * b)))
