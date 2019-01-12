37fb9e1e6d3f713bbda473d5150b2691f413c670
Yuriy Syrovetskiy <cblp@cblp.su>
2019-01-12 10:28:08 +0300

    rectangle: Fix non-positive numbers in input

--- rectangle/Cli.hs
+++ rectangle/Cli.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TypeApplications #-}
+
 import           Data.Maybe (mapMaybe)
 import           System.Environment (getArgs, getProgName)
 import           Text.Read (readMaybe)
@@ -6,8 +8,9 @@ main :: IO ()
 main = do
     prog   <- getProgName
     args   <- getArgs
-    (a, b) <- case mapMaybe readMaybe args of
-        [a, b] -> pure (a, b :: Double)
-        _      -> fail ("Usage: " ++ prog ++ " NUMBER NUMBER")
+    (a, b) <- case mapMaybe (readMaybe @Double) args of
+        sides@[a, b] | length args == 2, all (> 0) sides ->
+            pure (a, b)
+        _ -> fail ("Usage: " ++ prog ++ " NUMBER NUMBER")
     putStrLn ("perimeter: " ++ show (2 * (a + b)))
     putStrLn ("diagonal: " ++ show (sqrt (a * a + b * b)))
